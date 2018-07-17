0c17554ded0ee0ceef6a03dc312ec975a2ece7a4
Dmitrii Raikov <supraeternity@timilink.ru>
2018-07-17 16:39:54 +0500

    временно отключены css-модули

--- money-exchange/config/webpack.config.dev.js
+++ money-exchange/config/webpack.config.dev.js
@@ -168,7 +168,26 @@ module.exports = {
                   localIdentName: '[name]__[local]___[hash:base64:5]'
                 }
               },
-              'postcss-loader'
+              {
+                loader: require.resolve('postcss-loader'),
+                options: {
+                  // Necessary for external CSS imports to work
+                  // https://github.com/facebookincubator/create-react-app/issues/2677
+                  ident: 'postcss',
+                  plugins: () => [
+                    require('postcss-flexbugs-fixes'),
+                    autoprefixer({
+                      browsers: [
+                        '>1%',
+                        'last 4 versions',
+                        'Firefox ESR',
+                        'not ie < 9', // React doesn't support IE8 anyway
+                      ],
+                      flexbox: 'no-2009',
+                    }),
+                  ],
+                },
+              },
             ]
           },
           {
